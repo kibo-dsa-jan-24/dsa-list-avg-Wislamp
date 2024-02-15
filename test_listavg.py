c9e78982eb1dc2b25ac4d33e4034ae405e14a1bb
Wisdom A Dzeagu <wisdom.amartey@kibo.school>
2024-02-15 16:46:22 +0000

    getting average list

--- test_listavg.py
+++ test_listavg.py
@@ -7,3 +7,12 @@ class TestListAverage(unittest.TestCase):
         assert lavg.compute_avg() == 2.0
 
     # add more unit tests below
+
+    def test_faster(self):
+        lavg = ListAverage([1, 2, 3])
+        assert lavg.compute_avg_faster() == 2.0
+    
+    def test_faster_with_add(self):
+        lavg = ListAverage([1, 2, 3])
+        lavg.add(6)
+        assert lavg.compute_avg_faster() == 3.0
